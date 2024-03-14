7d636c19fccc3aca74ad135d6ce0b00117b2837e
97vack <1739717921@qq.com>
2024-03-14 10:56:22 +0800

    chore(release): optimize performance

--- docs/demos/_dymic.tsx
+++ docs/demos/_dymic.tsx
@@ -4,7 +4,7 @@ import { useForm } from 'react-form-simple';
 import { getUuid } from 'react-form-simple/utils/util';
 
 export default function App() {
-  const { model, render, validate } = useForm({
+  const { model, render, validate, forceUpdate } = useForm({
     fieldsDymic: [{ value: 0, uid: getUuid() }],
   });
   const { fieldsDymic } = model;
@@ -21,6 +21,7 @@ export default function App() {
         style={{ height: '30px', marginLeft: '10px' }}
         onClick={() => {
           model.fieldsDymic.splice(i, 1);
+          forceUpdate()
         }}
       >
         Delete
@@ -35,6 +36,7 @@ export default function App() {
         onClick={() => {
           const len = model.fieldsDymic.length;
           model.fieldsDymic.push({ value: len, uid: getUuid() });
+          forceUpdate()
         }}
       >
         add
